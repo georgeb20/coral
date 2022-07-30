629bdc6265f0ed9239b839e2129116a5e0a3ccaf
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-07-30 14:43:09 -0500

    Update gpio37.py

--- gpio37.py
+++ gpio37.py
@@ -1,10 +1,11 @@
 from periphery import GPIO
-
+import time
 solenoid = GPIO("/dev/gpiochip2", 13, "out")  # pin 37
 
 try:
   while True:
     solenoid.write(True)
+    time.sleep(1)
 finally:
   solenoid.write(False)
   solenoid.close()
