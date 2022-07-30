f9343d8e20ca59d56c46cdf98ae9378db96e3bb2
georgeb20 <89757049+georgeb20@users.noreply.github.com>
2022-07-30 13:12:55 -0500

    Create s.py

---
+++ s.py
@@ -0,0 +1,16 @@
+from periphery import PWM
+
+# Open PWM chip 0, channel 10
+pwm = PWM(2, 0)
+
+# Set frequency to 1 kHz
+pwm.frequency = 1e3
+# Set duty cycle to 75%
+pwm.duty_cycle = 0.75
+
+pwm.enable()
+
+# Change duty cycle to 50%
+pwm.duty_cycle = 0.50
+
+pwm.close()
